3d86bd3991941734a7315495bb046a002fa60475
Kike <kike.jurado2000@gmail.com>
2019-02-06 18:01:40 +0100

    Commit de POJO2.java al repositorio local

---
+++ POJO2.java
@@ -0,0 +1,17 @@
+public class POJO2{
+private String nombre;
+private String color;
+private String casa;
+
+
+	public String getNombre(){
+	return nombre;
+}
+	public void setNombre(String nombre){
+	this.nombre = nombre;
+}
+
+
+
+
+}
